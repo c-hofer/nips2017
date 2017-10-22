2156d6e4d950a6f70cfea405edf0e33cc3f2b81c
cdhofer <chofer@cosy.sbg.ac.at>
2017-10-22 12:00:09 +0200

    beautification of relative paths

--- mpeg7.py
+++ mpeg7.py
@@ -6,17 +6,19 @@ if __name__ == '__main__':
     from src.mpeg7.generate_dgm_provider import generate_dgm_provider
     from src.mpeg7.experiments import experiment
 
-    data_path = os.path.join(os.getcwd(), 'data/dgm_provider/npht_mpeg7_corrected_32dirs.h5')
-    if not os.path.isfile(data_path):
+    provider_path = os.path.join(os.path.dirname(__file__), 'data/dgm_provider/npht_mpeg7_corrected_32dirs.h5')
+    raw_data_path = os.path.join(os.path.dirname(__file__), 'data/raw_data/mpeg7_corrected')
+
+    if not os.path.isfile(provider_path):
         print('Persistence diagram provider does not exists, creating ... (this may need some time)')
         n_cores = max(1, cpu_count() - 1)
         generate_dgm_provider('./data/raw_data/mpeg7_corrected',
-                              data_path,
+                              raw_data_path,
                               32,
                               n_cores=n_cores)
     else:
         print('Found persistence diagram provider!')
 
     print('Starting experiment...')
-    experiment(data_path)
+    experiment(provider_path)
 
