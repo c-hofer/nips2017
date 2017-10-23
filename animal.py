d28554b82e56bdcb0fd7bf47271f04165ac844e0
cdhofer <chofer@cosy.sbg.ac.at>
2017-10-23 16:37:43 +0200

    changes for testing

--- animal.py
+++ animal.py
@@ -4,8 +4,8 @@ if __name__ == '__main__':
     from src.animal.generate_dgm_provider import generate_dgm_provider
     from src.animal.experiments import experiment
 
-    provider_path = os.path.join(os.path.dirname(__file__), 'data/dgm_provider/npht_animal_corrected_32dirs.h5')
-    raw_data_path = os.path.join(os.path.dirname(__file__), 'data/raw_data/animal_corrected')
+    provider_path = os.path.join(os.path.dirname(__file__), 'data/dgm_provider/npht_animal_32dirs.h5')
+    raw_data_path = os.path.join(os.path.dirname(__file__), 'data/raw_data/animal')
 
     if not os.path.isfile(provider_path):
         print('Persistence diagram provider does not exists, creating ... (this may need some time)')
